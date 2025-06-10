f52a014843cc75a12c9af8e0c27f3048e2643cda
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-09 17:02:36 -0700

    Include alt tag in movie poster

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -5,7 +5,7 @@ function MovieCard(props) {
   return (
     <article className="movie-card">
       <h2>{props.title}</h2>
-      <img src={props.poster}/>
+      <img src={props.poster} alt={`${props.title} Poster`}/>
       <p>{props.rateAvg}</p>
     </article>
   );
