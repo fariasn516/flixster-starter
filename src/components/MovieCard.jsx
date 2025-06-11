3e79355c81f360c93e43eb4da7f28a58f17633ce
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-11 10:21:54 -0700

    Implement like buttom

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,6 +4,7 @@ import MovieCardModal from "./MovieCardModal";
 
 function MovieCard(props) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLiked, setIsLiked] = useState(false);
 
   const openMovieModal = () => {
     setIsModalOpen(true);
@@ -13,19 +14,25 @@ function MovieCard(props) {
     setIsModalOpen(false);
   };
 
+  const handleLike = (e) => {
+    e.stopPropagation();
+    setIsLiked(!isLiked);
+  };
+
   return (
     <>
       <article className="movie-card" onClick={openMovieModal}>
         <h2>{props.title}</h2>
         <img src={props.poster} alt={`${props.title} Poster`} />
         <p>Rating: {props.rateAvg}/10</p>
+        <button onClick={handleLike} class="movie-like-button">{isLiked ? `♥️` : `♡`}</button>
       </article>
 
       <MovieCardModal
         isOpen={isModalOpen}
         onClose={closeMovieModal}
         movieId={props.id}
-        />
+      />
     </>
   );
 }
